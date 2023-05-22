2af3d9ca8c62c303e6dafaa05c966ff5bacf4020
AlissonCamposMar <alisson.camposm@uqvirtual.edu.co>
2023-05-22 17:27:14 -0500

    eliminación de clases

--- src/main/java/org/example/algorithms/Algorithms.java
+++ src/main/java/org/example/algorithms/Algorithms.java
@@ -22,40 +22,4 @@ public class Algorithms {
         _4AmericanoRecursivoDinamico.multiplicarArrayListRecursivo(arr1, arr2);
 
     }
-
-    public static void russianMultiplicationIterative (int num1, int num2)
-    {
-        RussianMultiplicationIterative.russianMultiply(num1,num2);
-    }
-
-    public static void hinduMultiplicationStatic (int num1, int num2)
-    {
-        HinduMultiplicationStatic.hinduMultiplication(num1,num2);
-    }
-
-    public static void egyptianMultiplicationStatic (int num1, int num2)
-    {
-        EgyptianMultiplicationStatic.egyptianMultiplication(num1,num2);
-    }
-
-    public static void karatsubaMultiplicationStatic (int num1, int num2)
-    {
-        KaratsubaMultiplicationStatic.karatsubaMultiplication(num1,num2);
-    }
-
-    public static void stringMultiplication (String num1, String num2)
-    {
-        StringMultiplication.stringMultiplication(num1,num2);
-    }
-
-    public static void divideConquerOne (String num1, String num2)
-    {
-        DivideConquerOne.multiply(num1,num2);
-    }
-
-    public static void divideConquerTwo (BigInteger num1, BigInteger num2)
-    {
-        DivideConquerTwo.divideConquerTwo(num1,num2);
-    }
-
 }
